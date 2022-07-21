419197380b620997cf0a6df3fb1e7bdd4c2301b4
cha-ji <chajiwon100785@gmail.com>
2022-07-21 13:17:30 +0900

    Feat: main activity

---
+++ app/src/main/java/com/sulgorae/crypto/MenuActivity.java
@@ -0,0 +1,20 @@
+package com.sulgorae.crypto;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+
+        // 타이틀바에 비트코인 아이콘 추가
+        getSupportActionBar().setIcon(R.drawable.app_bar);
+        getSupportActionBar().setDisplayUseLogoEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+    }
+}
