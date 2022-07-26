0ad8a76e888cf8f7ca8b9c9fe43ead40e2a9f91f
차지원 <chajiwon100785@gmail.com>
2022-07-26 20:57:35 +0900

    Feat: add api url [#3]

--- remote/src/main/java/com/sulgorae/remote/ApiService.kt
+++ remote/src/main/java/com/sulgorae/remote/ApiService.kt
@@ -3,7 +3,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 object ApiService {
 
-    private const val BASE_URL = "" // TODO: upbit url
+    private const val BASE_URL = "https://api.upbit.com/v1" // TODO: v1을 빼야할수도
 
     private val retrofit by lazy {
         Retrofit.Builder()
