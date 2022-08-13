9b762493163a559008b9d7e7722a9cd14472ce2f
차지원 <chajiwon100785@gmail.com>
2022-08-13 13:23:32 +0900

    Feat: menu viewmodel & factory [#15]

---
+++ app/src/main/java/com/sulgorae/crypto/factory/ViewModelFactory.kt
@@ -0,0 +1,20 @@
+package com.sulgorae.crypto.factory
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.sulgorae.crypto.menu.MenuViewModel
+import com.sulgorae.domain.datasource.ExchangeDataSource
+import com.sulgorae.domain.datasource.QuotationDataSource
+
+class ViewModelFactory private constructor(
+    private val exchangeDataSource: ExchangeDataSource,
+    private val quotationDataSource: QuotationDataSource
+) : ViewModelProvider.NewInstanceFactory() {
+
+    override fun <T : ViewModel> create(modelClass: Class<T>) = with(modelClass) {
+        when {
+            isAssignableFrom(MenuViewModel::class.java) -> MenuViewModel(exchangeDataSource, quotationDataSource)
+            else -> throw IllegalArgumentException("Unknown ViewModel class: ${modelClass.name}")
+        }
+    } as T
+}
--- app/src/main/java/com/sulgorae/crypto/menu/MenuViewModel.kt
+++ app/src/main/java/com/sulgorae/crypto/menu/MenuViewModel.kt
@@ -1,4 +1,33 @@
 package com.sulgorae.crypto.menu
 
-class MenuViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.sulgorae.domain.datasource.ExchangeDataSource
+import com.sulgorae.domain.datasource.QuotationDataSource
+import kotlinx.coroutines.launch
+
+class MenuViewModel(
+    private val exchangeDataSource: ExchangeDataSource,
+    private val quotationDataSource: QuotationDataSource
+) : ViewModel() {
+
+    private val _totalAccount = MutableLiveData(0.0)
+    val totalAccount: LiveData<Double> = _totalAccount
+
+    init {
+        setTotalAccount()
+    }
+
+    private fun setTotalAccount() {
+        viewModelScope.launch {
+            _totalAccount.value = exchangeDataSource.getAccounts().fold(0.0) { acc, account ->
+                acc + account.balance * when (account.currency) {
+                    "KRW" -> account.balance
+                    else -> quotationDataSource.getTicker(account.unitCurrency + "-" + account.currency).tradePrice
+                }
+            }
+        }
+    }
 }
