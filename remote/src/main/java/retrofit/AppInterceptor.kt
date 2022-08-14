6824e7a432220479734412bd05094840771c5a10
차지원 <chajiwon100785@gmail.com>
2022-08-14 16:38:14 +0900

    Fix: modify secret key [#21]

--- remote/src/main/java/retrofit/AppInterceptor.kt
+++ remote/src/main/java/retrofit/AppInterceptor.kt
@@ -4,27 +4,10 @@ import com.auth0.jwt.JWT
 import com.auth0.jwt.algorithms.Algorithm
 import okhttp3.Interceptor
 import okhttp3.Response
-import java.io.FileReader
 import java.io.IOException
 import java.util.*
 
 internal class AppInterceptor : Interceptor {
-
-    // reference: https://docs.upbit.com/docs/create-authorization-request
-    private val getToken: String = runCatching {
-        FileReader(KEY_PATH).let {
-            val accessKey = it.readText()
-            val secretKey = it.readText()
-            val algorithm: Algorithm = Algorithm.HMAC256(secretKey)
-
-            JWT.create()
-                .withClaim("access_key", accessKey)
-                .withClaim("nonce", UUID.randomUUID().toString())
-                .sign(algorithm)
-        }
-    }.onFailure {
-        it.printStackTrace()
-    }.getOrThrow()
 
     @Throws(IOException::class)
     override fun intercept(chain: Interceptor.Chain): Response = with(chain) {
@@ -34,7 +17,24 @@ internal class AppInterceptor : Interceptor {
         proceed(header)
     }
 
+    // reference: https://docs.upbit.com/docs/create-authorization-request
+    // FIXME: 인증방법 고민
+    private val getToken: String = runCatching {
+        val accessKey = TEMP_ACCESS_KEY
+        val secretKey = TEMP_SECRET_KEY
+        val algorithm: Algorithm = Algorithm.HMAC256(secretKey)
+
+        JWT.create()
+            .withClaim("access_key", accessKey)
+            .withClaim("nonce", UUID.randomUUID().toString())
+            .sign(algorithm)
+    }.onFailure {
+        it.printStackTrace()
+    }.getOrThrow()
+
     companion object {
-        const val KEY_PATH = "key.txt"
+        const val KEY_PATH = "./key.txt"
+        const val TEMP_ACCESS_KEY = ""
+        const val TEMP_SECRET_KEY = ""
     }
 }
