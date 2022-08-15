a8f51d86648e3b04e1ed311c3b123204ad240c76
차지원 <chajiwon100785@gmail.com>
2022-08-15 19:47:51 +0900

    Feat: get day candle method [#18]

--- remote/src/main/java/com/sulgorae/remote/service/TickerService.kt
+++ remote/src/main/java/com/sulgorae/remote/service/TickerService.kt
@@ -1,5 +1,6 @@
 package com.sulgorae.remote.service
 
+import com.sulgorae.data.entity.candles.days.Days
 import com.sulgorae.data.entity.ticker.Ticker
 import retrofit2.http.GET
 import retrofit2.http.Query
@@ -7,5 +8,8 @@ import retrofit2.http.Query
 interface TickerService {
 
     @GET("/ticker")
-    suspend fun getTicker(@Query("market")market: String = "KRW-BTC"): Ticker
+    suspend fun getTicker(@Query("market") market: String = "KRW-BTC"): Ticker
+
+    @GET("candles/days")
+    suspend fun getDayCandle(@Query("market") market: String = "KRW-BTC"): Days
 }
