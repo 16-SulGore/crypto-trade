fe67a7e3e4620c1f1b6987230beefdba5bb5bf61
차지원 <chajiwon100785@gmail.com>
2022-08-15 20:29:51 +0900

    Feat: get day candle method [#18]

--- domain/src/main/java/com/sulgorae/domain/datasource/QuotationDataSource.kt
+++ domain/src/main/java/com/sulgorae/domain/datasource/QuotationDataSource.kt
@@ -1,8 +1,11 @@
 package com.sulgorae.domain.datasource
 
+import com.sulgorae.domain.entity.DayCandleEntity
 import com.sulgorae.domain.entity.TickerEntity
 
 interface QuotationDataSource {
 
     suspend fun getTicker(market: String): TickerEntity
+
+    suspend fun getDayCandle(market: String): DayCandleEntity
 }
--- remote/src/main/java/com/sulgorae/remote/datasource/QuotationRemoteDataSource.kt
+++ remote/src/main/java/com/sulgorae/remote/datasource/QuotationRemoteDataSource.kt
@@ -6,7 +6,13 @@ import com.sulgorae.remote.service.TickerService
 class QuotationRemoteDataSource(private val tickerService: TickerService) : QuotationDataSource {
 
     override suspend fun getTicker(market: String) = runCatching {
-        tickerService.getTicker(market)
+        tickerService.getTicker(market).toEntities()[0]
+    }.onFailure { throwable ->
+        throwable.printStackTrace()
+    }.getOrThrow()
+
+    override suspend fun getDayCandle(market: String) = runCatching {
+        tickerService.getDayCandle(market).toEntities()[0]
     }.onFailure { throwable ->
         throwable.printStackTrace()
     }.getOrThrow()
